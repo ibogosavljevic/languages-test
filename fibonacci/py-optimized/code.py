228c2ffcde57de3b1e8df7da6bc0cb2043fe3983
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2024-12-01 22:09:37 +0100

    Optimized Python version

---
+++ fibonacci/py-optimized/code.py
@@ -0,0 +1,22 @@
+import sys
+
+def fibonacci(n):
+  if (n == 0):
+    return 0
+  if (n == 1):
+    return 1
+  return fibonacci(n-1) + fibonacci(n-2)
+
+def main():
+    u = int(sys.argv[1])
+    fib0 = 0
+    fib1 = 1
+    r = 0
+    for i in range(2, u):
+      fib = fib0 + fib1
+      r += fib
+      fib0 = fib1
+      fib1 = fib
+    print(r)
+
+main()
